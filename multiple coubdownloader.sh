8631a217202d062151cbfb24ef6029c3cc24e3f0
beellz <belalsayed@belals-MacBook-Pro.local>
2018-09-17 18:46:03 +0530

    adding neccesary comments

--- multiple coubdownloader.sh
+++ multiple coubdownloader.sh
@@ -1,9 +1,14 @@
-
 #!/bin/sh
-var=`cat file.txt`
-for i in $var; do
+
+#make a file with file.txt 
+#add all the coub share link in it .
+#file name will start accoring to the content
+
+
+var=`cat file.txt` #making a loop with var
+for i in $var; do 
 	  set -e
- youtube-dl -o 1.mp4 "$i"
+ youtube-dl -o 1.mp4 "$i" 
  youtube-dl -f html5-audio-high -o 1.mp3 "$i"
  printf '\x00\x00' | dd of=1.mp4 bs=1 count=2 conv=notrunc
 
